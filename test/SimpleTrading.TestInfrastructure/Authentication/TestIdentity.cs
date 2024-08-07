c5ce27b485210a34f560da017dbb0af596711d6e
Stefan <stefan.jeitler@outlook.com>
2024-08-07 18:38:20 +0200

    test: fix local test infrastructure
    appsettings.json in TestInfrastructure renamed to testsettings.json to avoid conflicts.
    Now, it should work with VisualStudio and Rider

--- test/SimpleTrading.TestInfrastructure/Authentication/TestIdentity.cs
+++ test/SimpleTrading.TestInfrastructure/Authentication/TestIdentity.cs
@@ -11,7 +11,7 @@ public static class TestIdentity
     {
         var config = new ConfigurationBuilder()
             .SetBasePath(AppContext.BaseDirectory)
-            .AddJsonFile("appsettings.json")
+            .AddJsonFile("testsettings.json")
             .AddUserSecrets<ClientCredentialsFlow>()
             .AddEnvironmentVariables()
             .Build();
