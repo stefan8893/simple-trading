d3ee27fad8bf6fc1bc64a1463d47643abd1ed67d
Stefan <stefan.jeitler@outlook.com>
2024-09-16 13:54:11 +0200

    feat: add user settings controller
    With one endpoint to get the current time in the user's timezone

--- src/SimpleTrading.WebApi/Features/UserSettings/UserSettingsController.cs
+++ src/SimpleTrading.WebApi/Features/UserSettings/UserSettingsController.cs
@@ -1,5 +1,23 @@
-﻿namespace SimpleTrading.WebApi.Features.UserSettings;
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SimpleTrading.Domain.Infrastructure;
+using SimpleTrading.WebApi.Infrastructure;
 
-public class UserSettingsController
+namespace SimpleTrading.WebApi.Features.UserSettings;
+
+[ApiController]
+[Route("[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+[SwaggerUiControllerPosition(6)]
+public class UserSettingsController : ControllerBase
 {
+    [AllowAnonymous]
+    [HttpGet("local-now", Name = "GetUserLocalNow")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult> GetLocalNow([FromServices] LocalNow localNow)
+    {
+        var nowInUserTimeZone = await localNow();
+        return Ok(nowInUserTimeZone);
+    }
 }
