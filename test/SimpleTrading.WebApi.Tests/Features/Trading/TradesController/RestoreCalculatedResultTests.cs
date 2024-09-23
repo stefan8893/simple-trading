b69aa62913cd904e0034ea516ca3c6de3b745e04
Stefan <stefan.jeitler@outlook.com>
2024-09-23 10:53:16 +0200

    refactor: replace mock with a stub

--- test/SimpleTrading.WebApi.Tests/Features/Trading/TradesController/RestoreCalculatedResultTests.cs
+++ test/SimpleTrading.WebApi.Tests/Features/Trading/TradesController/RestoreCalculatedResultTests.cs
@@ -2,23 +2,24 @@
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
-using Moq;
 using SimpleTrading.Client;
 using SimpleTrading.Domain.Infrastructure;
 using SimpleTrading.Domain.Trading;
 using SimpleTrading.Domain.Trading.UseCases.RestoreCalculatedResult;
 using SimpleTrading.Domain.Trading.UseCases.Shared;
 using SimpleTrading.TestInfrastructure;
+using SimpleTrading.WebApi.Tests.Features.Trading.TradesController.TestDoubles;
 
 namespace SimpleTrading.WebApi.Tests.Features.Trading.TradesController;
 
-public class RestoreCalculatedResultTests(TestingWebApplicationFactory<Program> factory) : WebApiTests(factory)
+public class RestoreCalculatedResultTests(
+    TestingWebApplicationFactory<Program> factory,
+    RestoreCalculatedResultInteractorStub restoreCalculatedResultInteractorStub)
+    : WebApiTests(factory), IClassFixture<RestoreCalculatedResultInteractorStub>
 {
-    private static readonly Mock<IRestoreCalculatedResult> InteractorMock = new (MockBehavior.Strict);
-
     protected override void OverrideServices(HostBuilderContext ctx, ContainerBuilder builder)
     {
-        builder.Register<IRestoreCalculatedResult>(_ => InteractorMock.Object);
+        builder.Register<IRestoreCalculatedResult>(_ => restoreCalculatedResultInteractorStub);
     }
 
     [Fact]
@@ -27,11 +28,9 @@ public class RestoreCalculatedResultTests(TestingWebApplicationFactory<Program> 
         // arrange
         var client = await CreateClient();
         var tradeId = Guid.Parse("8614528d-0d7b-4a62-b210-493eca25cf92");
-        InteractorMock.Setup(interactor =>
-                interactor.Execute(It.Is<RestoreCalculatedResultRequestModel>(x => x.TradeId == tradeId)))
-            .ReturnsAsync(
-                new Completed<RestoreCalculatedResultResponseModel>(
-                    new RestoreCalculatedResultResponseModel(tradeId, ResultModel.Loss, 55)));
+
+        restoreCalculatedResultInteractorStub.ResponseModel = new Completed<RestoreCalculatedResultResponseModel>(
+            new RestoreCalculatedResultResponseModel(tradeId, ResultModel.Loss, 55));
 
         // act
         var result = await client.RestoreCalculatedResultAsync(tradeId);
@@ -49,9 +48,7 @@ public class RestoreCalculatedResultTests(TestingWebApplicationFactory<Program> 
         // arrange
         var client = await CreateClient();
         var tradeId = Guid.Parse("8614528d-0d7b-4a62-b210-493eca25cf92");
-        InteractorMock.Setup(interactor =>
-                interactor.Execute(It.IsAny<RestoreCalculatedResultRequestModel>()))
-            .ReturnsAsync(new NotFound<Trade>(tradeId));
+        restoreCalculatedResultInteractorStub.ResponseModel = new NotFound<Trade>(tradeId);
 
         // act
         var act = () => client.RestoreCalculatedResultAsync(tradeId);
@@ -69,13 +66,11 @@ public class RestoreCalculatedResultTests(TestingWebApplicationFactory<Program> 
         // arrange
         var client = await CreateClient();
         var tradeId = Guid.Parse("8614528d-0d7b-4a62-b210-493eca25cf92");
-        InteractorMock.Setup(interactor =>
-                interactor.Execute(It.IsAny<RestoreCalculatedResultRequestModel>()))
-            .ReturnsAsync(new BusinessError(tradeId, "Something went badly wrong."));
+        restoreCalculatedResultInteractorStub.ResponseModel = new BusinessError(tradeId, "Something went badly wrong.");
 
         // act
         var act = () => client.RestoreCalculatedResultAsync(tradeId);
-        
+
         // assert
         var unprocessableEntity = await act.Should().ThrowExactlyAsync<SimpleTradingClientException<ErrorResponse>>();
         unprocessableEntity.Which.StatusCode.Should().Be(StatusCodes.Status422UnprocessableEntity);
