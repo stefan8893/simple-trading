4e6f3aca8ccf0d3c5d5a35f5aad493cc8b0626cc
Stefan <stefan.jeitler@outlook.com>
2024-09-24 09:39:08 +0200

    refactor: use 'init' rather than 'set' access modifier for currency's name

--- src/SimpleTrading.Domain/Trading/Currency.cs
+++ src/SimpleTrading.Domain/Trading/Currency.cs
@@ -5,7 +5,7 @@ namespace SimpleTrading.Domain.Trading;
 public class Currency : IEntity
 {
     public required string IsoCode { get; init; }
-    public required string Name { get; set; }
+    public required string Name { get; init; }
     public required Guid Id { get; init; }
     public required DateTime Created { get; init; }
 }
