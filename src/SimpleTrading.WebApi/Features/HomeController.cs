676ef2e17a5ba15fd97587065e9c029f833bfa96
Stefan <stefan.jeitler@outlook.com>
2025-08-05 14:43:25 +0200

    feat: remove exception test

--- src/SimpleTrading.WebApi/Features/HomeController.cs
+++ src/SimpleTrading.WebApi/Features/HomeController.cs
@@ -38,12 +38,4 @@ public class HomeController(IHostEnvironment hostEnvironment, IConfiguration con
 
         return Ok(apiInfo);
     }
-
-    [AllowAnonymous]
-    [ApiExplorerSettings(IgnoreApi = true)]
-    [HttpGet("exception-test", Name = nameof(ExceptionTest))]
-    public ActionResult<string> ExceptionTest()
-    {
-        throw new InvalidOperationException("Test exception");
-    }
 }
