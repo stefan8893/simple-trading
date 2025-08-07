593e35452306c8053aa2b81934c79604032edb53
Stefan <stefan.jeitler@outlook.com>
2025-08-07 15:27:19 +0200

    test: I managed to get source-generator analyzer tests to work

--- test/SimpleTrading.Domain.Generators.Tests/InteractorDiagnosticsTests.cs
+++ test/SimpleTrading.Domain.Generators.Tests/InteractorDiagnosticsTests.cs
@@ -7,19 +7,13 @@ namespace SimpleTrading.Domain.Generators.Tests;
 public class InteractorDiagnosticsTests
 {
     [Fact]
-    public void DummyTest_remove_me_when_other_tests_are_running()
-    {
-        Assert.True(true, "A Test project without a running test will fail");
-    }
-    
-    [Fact(Skip = "Under Construction")]
     public async Task Interactors_must_have_the_Interactor_suffix()
     {
-        var expectedDiagnostic = new DiagnosticResult("ST0001", DiagnosticSeverity.Error);
-        expectedDiagnostic
-            .WithSpan(14, 14, 14, 52)
+        var expectedDiagnostic = new DiagnosticResult("ST0001", DiagnosticSeverity.Error)
             .WithMessage(
-                "GetFoobarInteractorWithoutProperSuffix must end with 'Interactor', since it implements 'IInteractor'");
+                "GetFoobarInteractorWithoutProperSuffix must end with 'Interactor', since it implements 'IInteractor'")
+            .WithArguments("GetFoobarInteractorWithoutProperSuffix")
+            .WithLocation("/0/Test0.cs", 14, 14);
 
         var diagnosticsTest = new CSharpSourceGeneratorTest<InteractorProxyGenerator, XunitV3Verifier>
         {
