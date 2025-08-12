8008aaa00ee82a8448a8ca9f4eb11096e4f4622c
Stefan <stefan.jeitler@outlook.com>
2025-08-12 07:32:23 +0200

    refactor: obsolete methods in TradeRepository.cs removed

--- src/SimpleTrading.DataAccess/Repositories/TradeRepository.cs
+++ src/SimpleTrading.DataAccess/Repositories/TradeRepository.cs
@@ -16,43 +16,4 @@ public class TradeRepository(TradingDbContext dbContext) : RepositoryBase<Trade>
     {
         dbContext.References.RemoveRange(references);
     }
-
-    public Task<Asset?> FindAsset(Guid assetId)
-    {
-        return dbContext.Assets
-            .FindAsync(assetId)
-            .AsTask();
-    }
-
-    public async Task<Asset> GetAsset(Guid assetId)
-    {
-        var asset = await FindAsset(assetId);
-        return asset ?? throw new Exception($"{nameof(Asset)} not found.");
-    }
-
-    public Task<Profile?> FindProfile(Guid profileId)
-    {
-        return dbContext.Profiles
-            .FindAsync(profileId)
-            .AsTask();
-    }
-
-    public async Task<Profile> GetProfile(Guid profileId)
-    {
-        var profile = await FindProfile(profileId);
-        return profile ?? throw new Exception($"{nameof(Profile)} not found.");
-    }
-
-    public Task<Currency?> FindCurrency(Guid currencyId)
-    {
-        return dbContext.Currencies
-            .FindAsync(currencyId)
-            .AsTask();
-    }
-
-    public async Task<Currency> GetCurrency(Guid currencyId)
-    {
-        var currency = await FindCurrency(currencyId);
-        return currency ?? throw new Exception($"{nameof(Currency)} not found.");
-    }
 }
