6759f699d9cab1883356f3688cf5a2a2317823da
Stefan <stefan.jeitler@outlook.com>
2026-04-09 14:32:22 +0200

    fix: migrate trade's result column to json type

--- src/SimpleTrading.DataAccess.SqlServer/Migrations/20260409122203_Trade_Result_Migrate_to_JSON_Type.Designer.cs
+++ src/SimpleTrading.DataAccess.SqlServer/Migrations/20260409122203_Trade_Result_Migrate_to_JSON_Type.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SimpleTrading.DataAccess;
 
@@ -12,9 +13,11 @@ using SimpleTrading.DataAccess;
 namespace SimpleTrading.DataAccess.SqlServer.Migrations
 {
     [DbContext(typeof(TradingDbContext))]
-    partial class TradingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260409122203_Trade_Result_Migrate_to_JSON_Type")]
+    partial class Trade_Result_Migrate_to_JSON_Type
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/SimpleTrading.DataAccess.SqlServer/Migrations/20260409122203_Trade_Result_Migrate_to_JSON_Type.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimpleTrading.DataAccess.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Trade_Result_Migrate_to_JSON_Type : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Result",
+                table: "Trade",
+                type: "json",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
